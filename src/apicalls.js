ababd23ded0c4781274cbf4efeadc7b6ea42078c
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-09-20 16:06:26 -0400

    Add fetch call for phrase data

---
+++ src/apicalls.js
@@ -0,0 +1,9 @@
+function getPhrase() {
+  return fetch('https://techy-api.vercel.app/api/json')
+  .then(response => {
+    if(!response.ok) {
+      throw new Error (`${response.status}`)
+    }
+    return response.json()
+  })
+}
